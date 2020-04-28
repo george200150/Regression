fixed data normalization

Regression.py
@@ -4,7 +4,6 @@ Created on 24 apr. 2020
 @author: George
 '''
 
-import random
 import numpy as np
 from theano.sandbox.cuda.basic_ops import row
 
@@ -17,7 +16,7 @@ class MySGDRegression:
     def fit(self, x, y, learningRate = 0.001, noEpochs = 1000):
         self.coef_ = [0.0 for _ in range(len(x[0]) + 1)]    #beta or w coefficients y = w0 + w1 * x1 + w2 * x2 + ...
         # self.coef_ = [random.random() for _ in range(len(x[0]) + 1)]    #beta or w coefficients 
-        for epoch in range(noEpochs):
+        for _ in range(noEpochs):
             # TBA: shuffle the trainind examples in order to prevent cycles
             for i in range(len(x)): # for each sample from the training data
                 ycomputed = self.eval(x[i])     # estimate the output
@@ -31,8 +30,6 @@ class MySGDRegression:
 
     def eval(self, xi):
         yi = self.coef_[-1]
-        # intrebare: cand chemam predict dupa fit, nu cumva ultimul coeficient din coef_ nu este cel liber?
-        # De vreme ce am facut slice-ul coeficientilor la finalul fit-ului... 
         for j in range(len(xi)):
             yi += self.coef_[j] * xi[j]
         return yi 
@@ -76,13 +73,13 @@ class MyBatchRegression:
         @param batchSize: dimension of a batch - used to divide the dataset into smaller groups and train them simultaneously
         '''
         
-        if batchSize is None:
+        if batchSize is None: # define the size of a batch
             batchSize = len(y)
         
-        if theta is None:
+        if theta is None: # initialize the weights (equation variables)
             theta = np.random.randn(3,1)
         m = len(y)
-        #n_batches = int(m/batchSize)
+        #number_of_batches = int(m/batchSize)
         
         for _ in range(epochs):
             indices = np.random.permutation(m) # shuffle the data to avoid cycles
@@ -93,11 +90,10 @@ class MyBatchRegression:
                 y_i = y[i:i+batchSize]
                 
                 X_i = np.c_[np.ones(len(X_i)),X_i] # append columnwise column vector of ones to X batch (=> the shape of matrix we desire)
-               
+                
                 prediction = np.dot(X_i,theta)
-    
+                
                 theta = theta -(1/m)*eta*( X_i.T.dot((prediction - y_i)))
-
         
         self.intercept_ = theta[-1][0]
         self.coef_ = [coef[0] for coef in theta[:-1]]

regresieGBP.py
@@ -9,7 +9,7 @@ Created on 24 apr. 2020
 
 import Regression
 from sklearn.metrics.regression import mean_squared_error
-from normalizare import statisticalNormalisation
+from normalizare import stdNorm
 
 def plotData(x1, y1, x2 = None, y2 = None, x3 = None, y3 = None, title = None):
     plt.plot(x1, y1, 'ro', label = 'train data')
@@ -22,9 +22,10 @@ def plotData(x1, y1, x2 = None, y2 = None, x3 = None, y3 = None, title = None):
     plt.show()
 
 from random import seed
-import os, numpy as np
+import os
+import numpy as np
 from utils import *
-from random import seed
+
 
 # *two hundred plots later*
 def fun():
@@ -41,17 +42,28 @@ def fun():
     trainSample = np.random.choice(indexes, int(0.8 * len(inputs)), replace = False)
     testSample = [i for i in indexes  if not i in trainSample]
     
-    #NORMALIZATION OF TRAIN DATA
-    trainInputs = [inputs[i] for i in trainSample]
-    trainInputs = statisticalNormalisation(trainInputs)
-    trainOutputs = [outputs[i] for i in trainSample]
-    trainOutputs = statisticalNormalisation(trainOutputs)
     
-    #NORMALIZATION OF TEST DATA
+    trainInputs = [inputs[i] for i in trainSample]
     testInputs = [inputs[i] for i in testSample]
-    testInputs = statisticalNormalisation(testInputs)
+    
+    trainOutputs = [outputs[i] for i in trainSample]
     testOutputs = [outputs[i] for i in testSample]
-    testOutputs = statisticalNormalisation(testOutputs)
+    
+    norm = stdNorm()
+    featuresComplet = []
+    for feat in trainInputs:
+        featuresComplet.append(feat)
+    for feat in testInputs:
+        featuresComplet.append(feat)
+    '''for feat in trainOutputs:
+        featuresComplet.append(feat)
+    for feat in testOutputs:
+        featuresComplet.append(feat)'''
+    norm.statisticalNormalisation(featuresComplet)
+    #NORMALIZATION OF TRAIN DATA
+    trainInputs = norm.statisticalNormalisation(trainInputs)
+    #NORMALIZATION OF TEST DATA
+    testInputs = norm.statisticalNormalisation(testInputs)
     
     
     plotDataHistogram(trainInputs+testInputs, 'Capita GDP')
@@ -66,7 +78,7 @@ def fun():
     #regressor = linear_model.LinearRegression()
     #regressor = regression.MyLinearUnivariateRegression()
     #regressor = Regression.MySGDRegression()
-    regressor = Regression.MyBatchRegression()
+    regressor = Regression.MySGDRegression()
     regressor.fit(xx, trainOutputs) # FIT SINGLE MATRIX of noSamples x noFeatures
     
     w0, w1 = regressor.intercept_, regressor.coef_[0]